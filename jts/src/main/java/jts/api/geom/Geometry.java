4c67af03d088a929932c60243c1be6be9e5fa046
Martin Davis <mtnclimb@gmail.com>
2015-08-14 22:58:40 -0700

    Basic geometry interfaces

--- jts/src/main/java/jts/api/geom/Geometry.java
+++ jts/src/main/java/jts/api/geom/Geometry.java
@@ -1,5 +1,40 @@
 package jts.api.geom;
 
-public interface Geometry {
+import jts.api.geom.util.CoordVisitor;
+import jts.api.geom.util.GeometryVisitor;
 
+public interface Geometry {
+	
+	// metadata methods
+	GeometryFactory getFactory();
+	Realm getRealm();
+	
+	// topological methods
+	boolean isEmpty();
+	boolean isClosed();
+	int getDimension();
+	int getBoundaryDimension();
+	Geometry getBoundary();
+	
+	// metric methods
+	double getArea();
+	double getLength();
+	Envelope getEnvelope();
+	
+	// structural methods
+	int getNumGeometries();
+	Geometry getGeometryN(int n);
+	int getNumPoints();
+	
+	// client data
+	Object getData();
+	void setDate(Object data);
+	
+	void apply(CoordVisitor coordVisitor);
+	void apply(GeometryVisitor geomVisitor);
+	
+	// standard Object methods
+	String toString();
+	boolean equals();
+	int hashCode();
 }
